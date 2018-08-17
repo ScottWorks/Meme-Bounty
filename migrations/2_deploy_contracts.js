d190d9a03efb2ec17c9a7f19ec5dd38473671e17
ScottWorks <scottworks.io@gmail.com>
2018-08-16 20:23:46 -0600

    added library

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+const SafeMath = artifacts.require('./SafeMath.sol');
 const BountyBoard = artifacts.require('./BountyBoard.sol');
 
 module.exports = function(deployer) {
+  deployer.deploy(SafeMath);
+  deployer.link(SafeMath, BountyBoard);
   deployer.deploy(BountyBoard);
 };
