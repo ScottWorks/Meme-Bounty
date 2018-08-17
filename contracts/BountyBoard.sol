fb16945f1a5f712012469dc96df6f1ca49014a84
ScottWorks <scottworks.io@gmail.com>
2018-08-16 22:13:26 -0600

    added comments to contracts

--- contracts/BountyBoard.sol
+++ contracts/BountyBoard.sol
@@ -24,13 +24,23 @@ contract BountyBoard {
 // =================
 
 
-  
+    /** @dev multiplies value of two parameters provided
+    *   @param a - some number
+    *   @param b - some number
+    *   @return the result
+    */
+
     function setMath(uint a, uint b)
     public
     returns(uint)
     {
         c = a.mul(b);
     }
+
+
+    /** @dev provides the caller with the value of the c variable
+    *   @return the c variables state
+    */
 
     function getMath()
     public
@@ -39,6 +49,11 @@ contract BountyBoard {
     {
         return c;
     }
+
+    
+    /** @dev provides an array of bounty contract addresses
+    *   @return the bounty contract address array
+    */
 
     function getAllBountyAddresses() 
     public 
@@ -55,6 +70,14 @@ contract BountyBoard {
 // ==================
 
 
+
+    /** @dev initializes a bounty contract with the parameters provided then emits an event containing the address of the new contract
+    *   @param posterDeposit - specifies the amount the challenger will be awarded and the amount the poster must pay
+    *   @param description - specifies the challenge to be fulfilled by the challengers
+    *   @param voterDeposit - specifies the amount the voters must pay for upvotes
+    *   @param challengeDuration - specifies the amount of time challengers have to submit content
+    *   @param voteDuration - specifies the amount of time voters have to submit commits
+    */
 
     function createBountyContract(
         uint posterDeposit, 
